91626b0aff7656f3e845d59641e491c9b1c5d0bc
gyaresu <me@gareth.com.au>
2015-05-13 15:53:54 -0700

    added yeticss

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+var React = require('react');
+var style = require('./styles/main.styl');
+
+var Hello = React.createClass({
+    displayName: 'Hello',
+    render: function () {
+        return <div>Hello, {this.props.name}</div>;
+    }
+});
+
+React.render(<Hello name='World!'/>, document.body);
