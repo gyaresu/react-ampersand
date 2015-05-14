1204c30ed1a7d956490fcccf45185cd7fafbfc4f
gyaresu <me@gareth.com.au>
2015-05-13 21:48:28 -0700

    editorconfig, eslint / react modules to global, and rendering html

--- src/pages/public.js
+++ src/pages/public.js
@@ -3,6 +3,18 @@ import React from 'react';
 export default React.createClass({
     displayName: 'PublicPage',
     render () {
-        return <h1>Public page</h1>;
+        return ( // multi line JSX needs parentheses
+            <div className='container'>
+                <header role='banner'>
+                    <h1>Labelr</h1>
+                </header>
+                <div>
+                    <p>We label stuff for you because we can&trade;</p>
+                    <a className='button button-large' href='/repos'>
+                        <span className='mega-octicon octicon-mark-gitub'></span> Login with GitHub
+                    </a>
+                </div>
+            </div>
+        );
     }
 });
--- src/pages/repos.js
+++ src/pages/repos.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default React.createClass({
     displayName: 'ReposPage',
     render () {
-        return <h1>Repos page</h1>
+        return <h1>Repos page</h1>;
     }
-})
+});
